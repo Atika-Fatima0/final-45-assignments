047d2c499180b14a6df2a9b890598b5d7a3813c8
Atika Fatima <allaudina093@gmail.com>
2024-05-04 01:38:29 +0500

    first commit

---
+++ assignment 1 to 10/main.js
@@ -0,0 +1,68 @@
+//assignment #1
+//install node js,typescript and vscode on your computer
+//assignment #2
+//storing a person name in a variable 
+var Personname = "Atika Fatima";
+//printing a massage to the person 
+console.log("Hello ".concat(Personname, " would you like to learn some python today?"));
+//assignment #3
+//storing aperson name in a vaiable
+var personName = "Atika Fatima";
+// printing a person name in lowercase
+console.log("lowercase:", personName.toLowerCase());
+// printing a person name in uppercase
+console.log("uppercase:", personName.toUpperCase());
+// printing a person name in titlecase
+console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
+//assignment #4
+//storing the famous quote and its author
+var quote = "Nothing is impossibe because impossible itself I AM POSSIBLE";
+var authorr = "Atika Fatima";
+//printing the massege
+console.log("\"".concat(quote, "\" - ").concat(authorr));
+//assignment #5
+//storing the famous quote and its author
+var famQuote = "A person who never made a mistake never tried anything new.";
+var author = "Albert Einstein";
+//printing the massege
+console.log(" ".concat(author, " once said, \"").concat(famQuote, "\""));
+var famousPerson = "Albert Einstein";
+var message = " ".concat(famousPerson, " once said, \"").concat(famQuote, "\"");
+console.log(message);
+//assignment #6
+var withWhiteSpace = "\t \n Atika Fatima \t \n";
+console.log("with White Space:", withWhiteSpace);
+var strippedName = withWhiteSpace.trim();
+console.log("stripped Name:", strippedName);
+//assignment #7
+//addition
+var additionResult = 5 + 3;
+console.log("addition Result:", additionResult);
+//subtraction
+var subtractionResult = 10 - 2;
+console.log("subtraction Result:", subtractionResult);
+//multiplication
+var multiplicationResult = 4 * 2;
+console.log("multiplication Result:", multiplicationResult);
+//division
+var divisionResult = 16 / 2;
+console.log("division Result:", divisionResult);
+//assignment #8
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//assignment #9
+var favoritedResult = 8;
+var messageResult = "My favorite number: ".concat(favoritedResult);
+console.log(messageResult);
+//ctrl + questionmark
+//assignment #10
+//this program adds two number and print the result
+//define to numbers
+var number1 = 10;
+var number2 = 20;
+//add two nmubers
+var result = number1 + number2;
+//print result
+console.log(result);
---
+++ assignment 1 to 10/main.ts
@@ -0,0 +1,132 @@
+//assignment #1
+//install node js,typescript and vscode on your computer
+
+//assignment #2
+
+//storing a person name in a variable 
+let Personname:string = "Atika Fatima";
+
+//printing a massage to the person 
+console.log(`Hello ${Personname} would you like to learn some python today?`);
+
+
+//assignment #3
+
+//storing aperson name in a vaiable
+
+let personName:string = "Atika Fatima" 
+
+// printing a person name in lowercase
+
+console.log("lowercase:", personName.toLowerCase());
+
+// printing a person name in uppercase
+
+console.log("uppercase:", personName.toUpperCase());
+
+// printing a person name in titlecase
+
+console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
+
+//assignment #4
+
+//storing the famous quote and its author
+
+let quote:string = "Nothing is impossibe because impossible itself I AM POSSIBLE"
+
+let authorr:string = "Atika Fatima";
+
+//printing the massege
+
+console.log(`"${quote}" - ${authorr}`);
+
+//assignment #5
+
+//storing the famous quote and its author
+
+let famQuote:string = "A person who never made a mistake never tried anything new."
+
+let author:string = "Albert Einstein";
+
+//printing the massege
+
+console.log(` ${author} once said, "${famQuote}"`);
+
+let famousPerson:string = "Albert Einstein";
+let message:string = ` ${famousPerson} once said, "${famQuote}"`; 
+
+console.log(message);
+
+//assignment #6
+let  withWhiteSpace:string = "\t \n Atika Fatima \t \n";
+
+console.log("with White Space:", withWhiteSpace);
+
+let strippedName:string = withWhiteSpace.trim();
+
+console.log("stripped Name:", strippedName);
+
+
+//assignment #7
+
+//addition
+let additionResult:number = 5+3;
+
+console.log("addition Result:",additionResult);
+
+//subtraction
+let subtractionResult:number = 10-2;
+
+console.log("subtraction Result:",subtractionResult);
+
+//multiplication
+let multiplicationResult:number = 4*2;
+
+console.log("multiplication Result:", multiplicationResult);
+
+//division
+let divisionResult:number = 16/2;
+console.log("division Result:", divisionResult);
+
+
+//assignment #8
+
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+//assignment #9
+
+let favoritedResult:number = 8; 
+
+let messageResult:string = `My favorite number: ${favoritedResult}`;
+
+console.log(messageResult);
+
+//ctrl + questionmark
+
+//assignment #10
+
+//this program adds two number and print the result
+
+//define to numbers
+let number1:number = 10;
+let number2:number = 20;
+//add two nmubers
+let result:number = number1 + number2;
+//print result
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
